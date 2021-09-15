1e76c59c2b1f4114a3a9d087da15e3190bff4145
nabilweq <nabeeltkanr@gmail.com>
2021-09-15 13:04:01 +0530

    uplad done

---
+++ server.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func uploadFile(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Uploading file......\n")
+
+	r.ParseMultipartForm(10 << 20)
+
+	file, handler, err := r.FormFile("myFile")
+	if err != nil {
+		fmt.Println("Error recieving file form data")
+		fmt.Println(err)
+		return
+	}
+
+	defer file.Close()
+	fmt.Printf("Uploaded file: %+v\n", handler.Filename)
+	fmt.Printf("File size: %+v\n", handler.Size)
+	fmt.Printf("MIME Headers: %+v\n", handler.Header)
+
+	tempFile, err := ioutil.TempFile("temp-images",
+		"upload-*.png")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer tempFile.Close()
+
+	fileBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	tempFile.Write(fileBytes)
+
+	fmt.Fprintf(w, "Succesfully  File\n")
+}
+
+func setupRoutes() {
+	http.HandleFunc("/upload", uploadFile)
+	http.ListenAndServe(":8080", nil)
+}
+
+func main() {
+	fmt.Println("Go file upload")
+	setupRoutes()
+}
